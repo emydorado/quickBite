85609aaca2e7c2bd2a7f7c00a4b603c03690c56f
25natalia <25natalia09garcia@gmail.com>
2025-04-06 14:16:57 -0500

    css login: mejoras

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,38 +2,34 @@ import './login.css';
 
 function LogIn() {
 	return (
-		<div className='login-container'>
-			<h1 className='login-title'>Welcome back!</h1>
-			<p className='login-subtitle'>Please Log In to continue</p>
+		<div className='login-wrapper'>
+			<div className='login-container'>
+				<h1 className='login-title'>Welcome back!</h1>
+				<p className='login-subtitle'>Please Log In to continue</p>
 
-			<label htmlFor='email' className='login-label'>
-				Email
-			</label>
-			<ul></ul>
-			<input type='email' name='email' id='email' placeholder='Example@quickbite.com' className='login-input' />
-			<ul></ul>
+				<label htmlFor='email' className='login-label'>
+					Email
+				</label>
+				<ul></ul>
+				<input type='email' id='email' className='login-input' placeholder='Example@quickbite.com' />
+				<ul></ul>
 
-			<label htmlFor='password' className='login-label'>
-				Password
-			</label>
-			<ul></ul>
-			<input
-				type='password'
-				name='password'
-				id='password'
-				placeholder='Your Password'
-				className='login-input-password'
-			/>
-			<ul></ul>
+				<label htmlFor='password' className='login-label'>
+					Password
+				</label>
+				<ul></ul>
+				<input type='password' id='password' className='login-input-password' placeholder='Your Password' />
+				<ul></ul>
 
-			<p className='login-forgot-password'>Forget password?</p>
+				<p className='login-forgot-password'>Forget password?</p>
 
-			<button className='login-button'>Log in</button>
+				<button className='login-button'>Log in</button>
 
-			<section className='login-signup'>
-				<p className='login-account'>Don't have an account? </p>
-				<p className='signup-button'>Sign Up</p>
-			</section>
+				<section className='login-signup'>
+					<p className='login-account'>Don't have an account? </p>
+					<p className='signup-button'>Sign Up</p>
+				</section>
+			</div>
 		</div>
 	);
 }
