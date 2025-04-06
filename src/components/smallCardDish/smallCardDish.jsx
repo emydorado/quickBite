3208e338dc76a1a0d01cdaf3a6a9471112d4ea20
Emy Dorado <mac@MacBook-Air-de-Emy.local>
2025-04-06 10:11:13 -0500

    feat: nuevo componente small card

---
+++ src/components/smallCardDish/smallCardDish.jsx
@@ -0,0 +1,15 @@
+import './smallCardDish.css';
+
+const SmallCardDish = ({ img, title, time }) => {
+	return (
+		<div id='smallCard'>
+			<img src={img} alt='dish picture' />
+			<div className='textContent'>
+				<h1 className='title'>{title} </h1>
+				<p className='time'>{time}</p>
+			</div>
+		</div>
+	);
+};
+
+export default SmallCardDish;
