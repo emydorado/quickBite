5562723ce49fd64a121e1a7eed1575ad8ee0a0d3
25natalia <25natalia09garcia@gmail.com>
2025-04-06 11:06:44 -0500

    css welcome: css casi listo en welcome page

--- src/pages/welcome/welcome.jsx
+++ src/pages/welcome/welcome.jsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
+import './welcome.css';
 
 function Welcome() {
 	return (
 		<>
-			<h1>Welcome</h1>
-			<img src={logo} alt='QuickBite-logo' style={{ width: '150px', height: 'auto' }} />
+			<h1 className='welcome-title'>Welcome</h1>
+			<img className='welcome-logo' src={logo} alt='QuickBite-logo' />
 			<Link to='/home'>Ir a home</Link>
-			<p>Cook delicious recipes with what’s in your fridge</p>
-			<button>Get Started</button>
-			<button>I already have an account</button>
+			<p className='welcome-slogan'>Cook delicious recipes with what’s in your fridge</p>
+			<button className='welcome-button-started'>Get Started</button>
+			<section className='welcome-login'>
+				<p className='welcome-already'>Already have an account? </p>
+				<p className='welcome-login-button'>Log in</p>
+			</section>
 		</>
 	);
 }
