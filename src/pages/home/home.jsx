567883d01f22dee89a7ff585f0bf11227b0e531f
Emy Dorado <mac@MacBook-Air-de-Emy.local>
2025-04-06 10:45:52 -0500

    feat: nuevo componente ingredientes

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,20 @@
 import BigCardDish from '../../components/bigCardDish/bigCardDish';
 import SmallCardDish from '../../components/smallCardDish/smallCardDish';
+import IngredientButton from '../../components/ingredientButton/ingredientButton';
 import { recipes } from '../../data/recipes';
+import { ingredientes } from '../../data/ingredients';
 
 function Home() {
 	console.log(recipes);
 
 	return (
 		<>
+			{ingredientes.map((ingrediente) => {
+				return (
+					<IngredientButton key={ingrediente.id} name={ingrediente.name} emoji={ingrediente.emoji}></IngredientButton>
+				);
+			})}
+
 			{recipes.map((recipe) => {
 				return <BigCardDish key={recipe.id} img={recipe.img} title={recipe.recipe_name} time={recipe.id}></BigCardDish>;
 			})}
