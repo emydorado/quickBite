504ad95d803c684d0c5b45596b9712d96ece461e
25natalia <25natalia09garcia@gmail.com>
2025-04-06 16:12:21 -0500

    css home: layout home

--- src/components/smallCardDish/smallCardDish.jsx
+++ src/components/smallCardDish/smallCardDish.jsx
@@ -3,7 +3,7 @@ import './smallCardDish.css';
 const SmallCardDish = ({ img, title, time }) => {
 	return (
 		<div id='smallCard'>
-			<img src={img} alt='dish picture' />
+			<img src={img} alt='dish picture' className='home-image' />
 			<div className='onlyText'>
 				<h1 className='title'>{title} </h1>
 				<p className='time'>{time}</p>
