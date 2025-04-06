73edd664cba0a876d1365aa23dbdcc5d7991297c
25natalia <25natalia09garcia@gmail.com>
2025-04-06 10:24:51 -0500

    chore signup: html signup

--- src/pages/signUp/signUp.jsx
+++ src/pages/signUp/signUp.jsx
@@ -1,7 +1,20 @@
 function SignUp() {
 	return (
 		<>
-			<h1>signUp</h1>
+			<h1>Sign Up</h1>
+			<p>Create your new QuickBite account</p>
+			<label htmlFor=''>Name</label>
+			<input type='email' name='' id='' placeholder='Jhon Doe' />
+			<label htmlFor=''>Email</label>
+			<input type='email' name='' id='' placeholder='Example@quickbite.com' />
+			<label htmlFor=''>Password</label>
+			<input type='password' name='' id='' placeholder='Your Password' />
+			<p>Forget password?</p>
+			<button>Log in</button>
+			<p>Already have an account? </p>
+			<button>Log in</button>
+			<p>By continue you agree to our</p>
+			<button>Terms & Privacy Policy</button>
 		</>
 	);
 }
