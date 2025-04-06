b7c5558fdd80f640ce14d454ac22dc6fc05e0c9a
Emy Dorado <mac@MacBook-Air-de-Emy.local>
2025-04-06 08:43:49 -0500

    feat: agregando datas

---
+++ src/data/categories.js
@@ -0,0 +1,26 @@
+export const categories = [
+	{
+		id: 1,
+		name: 'Breakfast',
+	},
+	{
+		id: 2,
+		name: 'Lunch',
+	},
+	{
+		id: 3,
+		name: 'Dinner',
+	},
+	{
+		id: 4,
+		name: 'Snack',
+	},
+	{
+		id: 5,
+		name: 'Drink',
+	},
+	{
+		id: 6,
+		name: 'Dessert',
+	},
+];
