6a6ad1728f09031dd0267f3ad28acb1f3bf04150
25natalia <25natalia09garcia@gmail.com>
2025-04-06 10:15:29 -0500

    chore welcome: html login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,16 @@
 function LogIn() {
 	return (
 		<>
-			<h1>logIn</h1>
+			<h1>Welcome back!</h1>
+			<p>Please Log In to continue</p>
+			<label htmlFor=''>Email</label>
+			<input type='email' name='' id='' placeholder='Example@quickbite.com' />
+			<label htmlFor=''>Password</label>
+			<input type='password' name='' id='' placeholder='Your Password' />
+			<p>Forget password?</p>
+			<button>Log in</button>
+			<p>Don’t have an account? </p>
+			<button>Sign Up</button>
 		</>
 	);
 }
