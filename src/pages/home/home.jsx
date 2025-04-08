e0e7a6f379be59fce8e92d41aa4baa566c31ad82
emydorado <145810874+emydorado@users.noreply.github.com>
2025-04-07 20:40:15 -0500

    css: home terminado en css

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,147 +2,51 @@ import SmallCardDish from '../../components/smallCardDish/smallCardDish';
 import NavMenu from '../../components/navMenu/navMenu';
 import { recipes } from '../../data/recipes';
 import './home.css';
-import BigCardDish from '../../components/bigCardDish/bigCardDish';
-function Home() {
+
+const Home = () => {
+	const sections = [
+		{
+			title: 'Recommended recipes',
+			recipeIndexes: [1, 2, 3, 4, 5, 6],
+		},
+		{
+			title: 'Lastest recipes made by you',
+			recipeIndexes: [7, 8, 9, 10, 11, 12],
+		},
+		{
+			title: 'Because you liked "Chickpea cookies"',
+			recipeIndexes: [13, 14, 15, 16, 17, 18],
+		},
+	];
+
 	return (
-		<>
-			<section className='home-container'>
-				<h1 className='home-title'>Explore recipes</h1>
+		<section className='home-container'>
+			<h1 className='home-title'>Explore recipes</h1>
 
-				<NavMenu></NavMenu>
+			<NavMenu />
 
-				<p className='home-subtitle'>Recommended recipes</p>
-				<section className='home-recommended'>
-					{recipes[1] && (
-						<SmallCardDish
-							key={recipes[1].id}
-							id={recipes[1].id}
-							img={recipes[1].img}
-							title={recipes[1].recipe_name}
-							time={recipes[1].time}
-						/>
-					)}
-
-					{recipes[2] && (
-						<SmallCardDish
-							key={recipes[2].id}
-							id={recipes[2].id}
-							img={recipes[2].img}
-							title={recipes[2].recipe_name}
-							time={recipes[2].id}
-						/>
-					)}
-					{recipes[3] && (
-						<SmallCardDish
-							key={recipes[3].id}
-							id={recipes[3].id}
-							img={recipes[3].img}
-							title={recipes[3].recipe_name}
-							time={recipes[3].id}
-						/>
-					)}
-					{recipes[4] && (
-						<SmallCardDish
-							key={recipes[4].id}
-							id={recipes[4].id}
-							img={recipes[4].img}
-							title={recipes[4].recipe_name}
-							time={recipes[4].id}
-						/>
-					)}
+			{sections.map(({ title, recipeIndexes }, i) => (
+				<section key={i}>
+					<p className='home-subtitle'>{title}</p>
+					<div className={`home-section home-section-${i}`}>
+						{recipeIndexes.map((index) => {
+							const recipe = recipes[index];
+							if (!recipe) return null;
+							return (
+								<SmallCardDish
+									key={recipe.id}
+									id={recipe.id}
+									img={recipe.img}
+									title={recipe.recipe_name}
+									time={recipe.id}
+								/>
+							);
+						})}
+					</div>
 				</section>
-
-				<p className='home-subtitle'>Lastest recipes made by you</p>
-				<section className='home-made-by-you'>
-					{recipes[5] && (
-						<SmallCardDish
-							id={recipes[5].id}
-							key={recipes[5].id}
-							img={recipes[5].img}
-							title={recipes[5].recipe_name}
-							time={recipes[5].id}
-						/>
-					)}
-					{recipes[6] && (
-						<SmallCardDish
-							key={recipes[6].id}
-							id={recipes[6].id}
-							img={recipes[6].img}
-							title={recipes[6].recipe_name}
-							time={recipes[6].id}
-						/>
-					)}
-					{recipes[7] && (
-						<SmallCardDish
-							key={recipes[7].id}
-							id={recipes[7].id}
-							img={recipes[7].img}
-							title={recipes[7].recipe_name}
-							time={recipes[7].id}
-						/>
-					)}
-					{recipes[8] && (
-						<SmallCardDish
-							key={recipes[8].id}
-							id={recipes[8].id}
-							img={recipes[8].img}
-							title={recipes[8].recipe_name}
-							time={recipes[8].id}
-						/>
-					)}
-				</section>
-
-				<p className='home-subtitle'>Because you liked "Chickpea cookies"</p>
-				<section className='home-because-you-like'>
-					{recipes[3] && (
-						<SmallCardDish
-							key={recipes[9].id}
-							id={recipes[9].id}
-							img={recipes[9].img}
-							title={recipes[9].recipe_name}
-							time={recipes[9].id}
-						/>
-					)}
-					{recipes[3] && (
-						<SmallCardDish
-							key={recipes[10].id}
-							id={recipes[10].id}
-							img={recipes[10].img}
-							title={recipes[10].recipe_name}
-							time={recipes[10].id}
-						/>
-					)}
-					{recipes[3] && (
-						<SmallCardDish
-							key={recipes[11].id}
-							id={recipes[11].id}
-							img={recipes[11].img}
-							title={recipes[11].recipe_name}
-							time={recipes[11].id}
-						/>
-					)}
-					{recipes[3] && (
-						<SmallCardDish
-							key={recipes[12].id}
-							id={recipes[12].id}
-							img={recipes[12].img}
-							title={recipes[12].recipe_name}
-							time={recipes[12].id}
-						/>
-					)}
-					{recipes[3] && (
-						<SmallCardDish
-							key={recipes[13].id}
-							id={recipes[13].id}
-							img={recipes[13].img}
-							title={recipes[13].recipe_name}
-							time={recipes[13].id}
-						/>
-					)}
-				</section>
-			</section>
-		</>
+			))}
+		</section>
 	);
-}
+};
 
 export default Home;
