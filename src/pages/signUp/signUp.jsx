3f13f10ebeb01a4aacfd19f306442b7d5b2f5b5f
25natalia <25natalia09garcia@gmail.com>
2025-04-09 09:25:35 -0500

    css signup: sign up listo sin layout ni responsive

--- src/pages/signUp/signUp.jsx
+++ src/pages/signUp/signUp.jsx
@@ -1,24 +1,52 @@
 import { useNavigate } from 'react-router-dom';
+import './signup.css';
 
 function SignUp() {
 	const navigate = useNavigate();
 
 	return (
 		<>
-			<h1>Sign Up</h1>
-			<p>Create your new QuickBite account</p>
-			<label htmlFor=''>Name</label>
-			<input type='email' name='' id='' placeholder='Jhon Doe' />
-			<label htmlFor=''>Email</label>
-			<input type='email' name='' id='' placeholder='Example@quickbite.com' />
-			<label htmlFor=''>Password</label>
-			<input type='password' name='' id='' placeholder='Your Password' />
-			<p>Forget password?</p>
-			<button onClick={() => navigate('/home')}>Sign in</button>
-			<p>Already have an account? </p>
-			<button onClick={() => navigate('/login')}>Log in</button>
-			<p>By continue you agree to our</p>
-			<button>Terms & Privacy Policy</button>
+			<div className='signup-wrapper'>
+				<h1 className='signup-title'>Sign Up</h1>
+				<p className='signup-subtitle'>Create your new QuickBite account</p>
+				<div>
+					<label className='signup-label ' htmlFor=''>
+						Name
+					</label>
+					<ul></ul>
+					<input type='email' className='signup-input' name='' id='' placeholder='Jhon Doe' />
+				</div>
+				<div>
+					<label className='signup-label ' htmlFor=''>
+						Email
+					</label>
+					<ul></ul>
+					<input type='email' className='signup-input' name='' id='' placeholder='Example@quickbite.com' />
+				</div>
+				<div>
+					<label className='signup-label ' htmlFor=''>
+						Password
+					</label>
+					<ul></ul>
+					<input type='password' className='signup-input' name='' id='' placeholder='Your Password' />
+				</div>
+				<div>
+					<p className='signup-forgot-password'>Forget password?</p>
+					<button className='signup-button' onClick={() => navigate('/home')}>
+						Sign in
+					</button>
+				</div>
+				<div className='signup-container-login'>
+					<p className='signup-already-text'>Already have an account? </p>
+					<p className='signup-login-button' onClick={() => navigate('/login')}>
+						Log in
+					</p>
+				</div>
+				<div className='signup-container-login'>
+					<p className='signup-already-text'>By continue you agree to our</p>
+					<p className='signup-login-button'>Terms & Privacy Policy</p>
+				</div>
+			</div>
 		</>
 	);
 }
