1a27b0d29f9a49618c1d84640128ffa47302cb4f
emydorado <145810874+emydorado@users.noreply.github.com>
2025-05-02 13:18:22 -0500

    css: medidas variables saved

--- src/pages/saved/saved.jsx
+++ src/pages/saved/saved.jsx
@@ -6,22 +6,24 @@ import './saved.css';
 function Saved() {
 	return (
 		<>
-			<NavMenu></NavMenu>
+			<div className='saved-container'>
+				<NavMenu></NavMenu>
 
-			<h1 className='saved-title'>Saved recipes</h1>
+				<h1 className='saved-title'>Saved recipes</h1>
 
-			<section id='saved-cards'>
-				{[3, 4, 5, 6, 7, 8].map((i) =>
-					recipes[i] ? (
-						<SavedCardDish
-							key={recipes[i].id}
-							img={recipes[i].img}
-							title={recipes[i].recipe_name}
-							time={recipes[i].prep_time_minutes}
-						/>
-					) : null
-				)}
-			</section>
+				<section id='saved-cards'>
+					{[3, 4, 5, 6, 7, 8].map((i) =>
+						recipes[i] ? (
+							<SavedCardDish
+								key={recipes[i].id}
+								img={recipes[i].img}
+								title={recipes[i].recipe_name}
+								time={recipes[i].prep_time_minutes}
+							/>
+						) : null
+					)}
+				</section>
+			</div>
 		</>
 	);
 }
