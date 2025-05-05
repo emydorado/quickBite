7807f97e52402d6c8b5929f6620e1027a4514e24
emydorado <145810874+emydorado@users.noreply.github.com>
2025-05-05 11:06:26 -0500

    feat: done recipes store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
+import doneRecipesReducer from './doneRecipes/doneRecipesSlice';
 import savedRecipesReducer from './savedRecipes/savedRecipesSlice';
 
 const preloadedState = {
 	savedRecipes: {
 		saved: JSON.parse(localStorage.getItem('savedRecipes')) || [],
 	},
+
+	doneRecipes: {
+		saved: JSON.parse(localStorage.getItem('doneRecipes')) || [],
+	},
 };
 
 export const store = configureStore({
 	reducer: {
 		savedRecipes: savedRecipesReducer,
+		doneRecipes: doneRecipesReducer,
 	},
 
 	preloadedState,
