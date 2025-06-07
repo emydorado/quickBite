d47944c2f8b57beb35a102ba70736ffca4aa6fc9
emydorado <145810874+emydorado@users.noreply.github.com>
2025-06-06 20:28:24 -0500

    fix: loader componente

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,12 +1,41 @@
-import './loader.css';
+import React from 'react';
+import { CircularProgress, Box, Typography } from '@mui/material';
 
-function Loader() {
+const Loader = () => {
 	return (
-		<div className='loader-fullscreen'>
-			<div className='spinner'></div>
-			<span className='loader-text'>Loading recipes...</span>
-		</div>
+		<Box
+			sx={{
+				position: 'fixed',
+				top: '50%',
+				left: '50%',
+				transform: 'translate(-50%, -50%)',
+				zIndex: 1000,
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
+			<CircularProgress
+				size={60}
+				thickness={5}
+				sx={{
+					color: '#8bc652',
+					marginBottom: 2,
+				}}
+			/>
+			<Typography
+				variant='h6'
+				sx={{
+					color: '#333',
+					fontWeight: 500,
+					fontFamily: '"Radio Canada", sans-serif',
+				}}
+			>
+				Loading...
+			</Typography>
+		</Box>
 	);
-}
+};
 
 export default Loader;
